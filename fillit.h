f66486df5cd0efc6a243c4abaa37c894cb0d41a0
CrushedElva <celva@DESKTOP-PP0QK2R.localdomain>
2019-10-04 22:52:43 +0300

    modified fillit.h fillit_read.c

--- fillit.h
+++ fillit.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fillit.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: celva <celva@student.42.fr>                +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/26 13:19:25 by dkathlee          #+#    #+#             */
-/*   Updated: 2019/10/03 13:50:04 by celva            ###   ########.fr       */
+/*   Updated: 2019/10/04 22:18:55 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,30 @@
 # include <fcntl.h>
 # include <stdlib.h>
 # include "libft/libft.h"
+# define figure_1_1 "#...#...#...#..."
+# define figure_1_2 "####............"
+# define figure_2_1 ".##.##.........."
+# define figure_2_2 "#...##...#......"
+# define figure_3_1 "##...##........."
+# define figure_3_2 ".#..##..#......."
+# define figure_4_1 "##..##.........."
+# define figure_5_1 "#...#...##......"
+# define figure_5_2 "..#.###........."
+# define figure_5_3 "##...#...#......"
+# define figure_5_4 "###.#..........."
+# define figure_6_1 "###..#.........."
+# define figure_6_2 "#...##..#......."
+# define figure_6_3 ".#..###........."
+# define figure_6_4 ".#..##...#......"
+# define figure_7_1 ".#...#..##......"
+# define figure_7_2 "#...###........."
+# define figure_7_3 "##..#...#......."
+# define figure_7_4 "###...#........."
+# define f_transit_2_1 "##.##..........."
+# define f_transit_3_2 "#..##..#........"
+# define f_transit_5_2 "#.###..........."
+# define f_transit_6_3 "#..###.........."
+# define f_transit_6_4 "#..##...#......."
+# define f_transit_7_1 "#...#..##......."
 
 #endif
--- fillit_read.c
+++ fillit_read.c
@@ -3,33 +3,166 @@
 /*                                                        :::      ::::::::   */
 /*   fillit_read.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: celva <celva@student.42.fr>                +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/02 18:41:58 by celva             #+#    #+#             */
-/*   Updated: 2019/10/03 14:03:44 by celva            ###   ########.fr       */
+/*   Updated: 2019/10/04 22:44:17 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+#include <stdio.h>
 
 int	check_figure(t_dlist **figures, char *str)
 {
+	int i;
+	int flag;
+	int m;
+
+	i = 0;
+	flag = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] != '.' && str[i] != '#')
+			return (-1);
+		if (str[i] == '#' && flag == 0)
+			m = i;
+		if (str[i] == '#')
+		{
+			str[i - m] = '#';
+			str[i] = '.';
+		}
+		i++;
+	}
+	if (ft_strcmp(str, figure_1_1) == 0)
+	{
+		//как-то добавление в список
+		return (0);
+	}
+	if (ft_strcmp(str, figure_1_2) == 0)
+	{
+		//как-то добавление в список
+		return (0);
+	}
+	if (ft_strcmp(str, f_transit_2_1) == 0)
+	{
+		//как-то добавление в список как figure_2_1
+		return (0);
+	}
+	if (ft_strcmp(str, figure_2_2) == 0)
+	{
+		//как-то добавление в список
+		return (0);
+	}
+	if (ft_strcmp(str, figure_3_1) == 0)
+	{
+		//как-то добавление в список
+		return (0);
+	}
+	if (ft_strcmp(str, f_transit_3_2) == 0)
+	{
+		//как-то добавление в список как figure_3_2
+		return (0);
+	}
+	if (ft_strcmp(str, figure_4_1) == 0)
+	{
+		//как-то добавление в список
+		return (0);
+	}
+	if (ft_strcmp(str, figure_5_1) == 0)
+	{
+		//как-то добавление в список
+		return (0);
+	}
+	if (ft_strcmp(str, f_transit_5_2) == 0)
+	{
+		//как-то добавление в список как figure_5_2
+		return (0);
+	}
+	if (ft_strcmp(str, figure_5_3) == 0)
+	{
+		//как-то добавление в список
+		return (0);
+	}
+	if (ft_strcmp(str, figure_5_4) == 0)
+	{
+		//как-то добавление в список
+		return (0);
+	}
+	if (ft_strcmp(str, figure_6_1) == 0)
+	{
+		//как-то добавление в список
+		return (0);
+	}
+	if (ft_strcmp(str, figure_6_2) == 0)
+	{
+		//как-то добавление в список
+		return (0);
+	}
+	if (ft_strcmp(str, f_transit_6_3) == 0)
+	{
+		//как-то добавление в список как figure_6_3
+		return (0);
+	}
+	if (ft_strcmp(str, f_transit_6_4) == 0)
+	{
+		//как-то добавление в список как figure_6_4
+		return (0);
+	}
+	if (ft_strcmp(str, f_transit_7_1) == 0)
+	{
+		//как-то добавление в список как figure_7_1
+		return (0);
+	}
+	if (ft_strcmp(str, figure_7_2) == 0)
+	{
+		//как-то добавление в список
+		return (0);
+	}
+	if (ft_strcmp(str, figure_7_3) == 0)
+	{
+		//как-то добавление в список
+		return (0);
+	}
+	if (ft_strcmp(str, figure_7_4) == 0)
+	{
+		//как-то добавление в список
+		return (0);
+	}
+	return (-1);
 }
 
 int	fillit_read(t_dlist **figures, char *fname)
 {
 	int		fd;
+	int		i;
 	char	*str;
 	char	*new;
 	
 	str = (char*)malloc(sizeof(char) * 17);
 	fd = open(fname, O_RDONLY);
 	new = str;
-	while (read(fd, str, 5) != 0)
+	while (1)
 	{
-		read(fd, str, 5);
-		if (str[4] != '\n')
+		i = 0;
+		while (i < 4)
+		{
+			if (read(fd, str, 5) == 0)
+				return (0);
+			if (str[4] != '\n')
+				return (-1);
+			str += 4;
+			i++;
+		}
+		if (check_figure(figures, str) == -1)
 			return (-1);
-		str += 4;
+		if (read(fd, str, 1) != 0 && *str != '\n')
+			return (-1);
 	}
 }
+
+/*int main()
+{
+	char *str = ".....#...##..#..";
+	printf("%s\n", check_figure(str));
+}*/
