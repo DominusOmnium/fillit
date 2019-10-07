4e564da5c4461319f0584ec703c9f4a743cf1575
CrushedElva <celva@DESKTOP-PP0QK2R.localdomain>
2019-10-07 22:10:54 +0300

    modif fillit_read.h

--- fillit_read.c
+++ fillit_read.c
@@ -6,12 +6,64 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/02 18:41:58 by celva             #+#    #+#             */
-/*   Updated: 2019/10/07 18:20:56 by marvin           ###   ########.fr       */
+/*   Updated: 2019/10/07 22:10:01 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include <stdio.h>
+
+int	check(t_point *m_point)
+{
+	int i;
+
+	i = 0;
+	while (i < 4)
+	{
+		if (i == 0 && (ABS(m_point[i].i - m_point[1].i) == 1 ||
+			ABS(m_point[i].i - m_point[3].i) == 1 || ABS(m_point[i].j -
+			m_point[1].j) == 1 || ABS(m_point[i].j - m_point[3].j) == 1))
+			i++;
+		else if (i == 3 && (ABS(m_point[i].i - m_point[2].i) == 1 ||
+			ABS(m_point[i].i - m_point[0].i) == 1 || ABS(m_point[i].j -
+			m_point[2].j) == 1 || ABS(m_point[i].j - m_point[0].j) == 1))
+			i++;
+		else if ((i == 2 || i == 1) && (ABS(m_point[i].i -
+			m_point[i - 1].i) == 1 || ABS(m_point[i].i - m_point[i + 1].i) == 1
+			|| ABS(m_point[i].j - m_point[i - 1].j) == 1 || ABS(m_point[i].j
+			- m_point[i + 1].j) == 1))
+			i++;
+		else
+			return (-1);
+	}
+}
+
+void	push_tetriminos(t_dlist **figures, t_point *m_point)
+{
+	t_dlist	*new;
+
+	new = *figures;
+	while (new != NULL)
+	{
+		if (((t_point*)(new->content))[0].i == m_point[0].i &&
+			((t_point*)(new->content))[0].j == m_point[0].j &&
+			((t_point*)(new->content))[1].i == m_point[1].i &&
+			((t_point*)(new->content))[1].j == m_point[1].j &&
+			((t_point*)(new->content))[2].i == m_point[2].i &&
+			((t_point*)(new->content))[2].j == m_point[2].j &&
+			((t_point*)(new->content))[3].i == m_point[3].i &&
+			((t_point*)(new->content))[3].j == m_point[3].j)
+		{
+			new->content_size += 1;
+			break ;
+		}
+	}
+	if (new == NULL)
+	{
+		ft_dlst_push_front(figures, ft_dlst_create_elem((void*)m_point));
+		(*figures)->content_size = 1;
+	}
+}
 
 int check_figure(t_dlist **figures, char *str)
 {
@@ -86,11 +138,4 @@ t_dlist *read_tetriminos(char fname)
 	t_dlist *figures;
 
 	fillit_read(&figures, fname);
-}
-
-int main()
-{
-	t_dlist *lst = NULL;
-	char str[] = ".....#...##..#..";
-	printf("%d\n", check_figure(&lst, str));
 }
