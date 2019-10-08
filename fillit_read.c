20d143702bc5f053fb698d528f0cfde63b914d7f
CrushedElva <celva@DESKTOP-PP0QK2R.localdomain>
2019-10-08 14:00:15 +0300

    modif fillit_read.c

--- fillit_read.c
+++ fillit_read.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/02 18:41:58 by celva             #+#    #+#             */
-/*   Updated: 2019/10/07 22:10:01 by marvin           ###   ########.fr       */
+/*   Updated: 2019/10/08 13:47:58 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 int	check(t_point *m_point)
 {
 	int i;
+
+	printf("check\n");
 
 	i = 0;
 	while (i < 4)
@@ -42,7 +44,10 @@ void	push_tetriminos(t_dlist **figures, t_point *m_point)
 {
 	t_dlist	*new;
 
+	printf("push_tetriminos\n");
+
 	new = *figures;
+		printf("p: %p\n", new);
 	while (new != NULL)
 	{
 		if (((t_point*)(new->content))[0].i == m_point[0].i &&
@@ -57,12 +62,15 @@ void	push_tetriminos(t_dlist **figures, t_point *m_point)
 			new->content_size += 1;
 			break ;
 		}
+		new = new->next;
 	}
 	if (new == NULL)
 	{
+		printf("push_tetriminos push\n");
 		ft_dlst_push_front(figures, ft_dlst_create_elem((void*)m_point));
 		(*figures)->content_size = 1;
 	}
+	printf("push_tetriminos end\n");
 }
 
 int check_figure(t_dlist **figures, char *str)
@@ -71,6 +79,8 @@ int check_figure(t_dlist **figures, char *str)
 	int		i;
 	int		j;
 	int		c;
+	
+	printf("check_figure\n");
 	
 	m_point = (t_point*)ft_memalloc(sizeof(t_point) * 4);
 	i = 0;
@@ -101,7 +111,7 @@ int check_figure(t_dlist **figures, char *str)
 		m_point[c].j = m_point[c].j - j;
 		c++;
 	}
-	ft_dlst_push_back(figures, ft_dlst_create_elem((void*)m_point));
+	push_tetriminos(figures, m_point);
 }
 
 int	fillit_read(t_dlist **figures, char *fname)
@@ -111,21 +121,36 @@ int	fillit_read(t_dlist **figures, char *fname)
 	char	*str;
 	char	*new;
 	
+	printf("fillit_read\n");
+	
 	str = ft_strnew(16);
 	fd = open(fname, O_RDONLY);
+	
+	printf("fd - %d\n", fd);
+	
 	new = str;
 	while (1)
 	{
+		str = new;
+		printf("infinite while\n");
 		i = 0;
 		while (i < 4)
 		{
-			if (read(fd, str, 5) == 0)
+			printf("while in fillit_read to 4\n");
+			if (read(fd, str, 6) == 0)
+			{
+				printf("read = 0\n");
 				return (0);
-			if (str[4] != '\n')
+			}
+			if (str[4] != 10)
+			{
+				//printf("%s", str);
 				return (-1);
+			}
 			str += 4;
 			i++;
 		}
+		printf("%s\n", new);
 		if (check_figure(figures, new) == -1)
 			return (-1);
 		if (read(fd, str, 1) != 0 && *str != '\n')	
@@ -133,9 +158,35 @@ int	fillit_read(t_dlist **figures, char *fname)
 	}
 }
 
-t_dlist *read_tetriminos(char fname)
+t_dlist *read_tetriminos(char *fname)
 {
 	t_dlist *figures;
 
-	fillit_read(&figures, fname);
+	figures = NULL;
+	printf("read_tetriminos\n");
+	if (fillit_read(&figures, fname) == -1)
+		return (NULL);
+	return (figures);
+}
+
+int main()
+{
+	t_dlist *pr;
+	char fname[9] = "Proverka";
+
+	pr = read_tetriminos(fname);
+	int i = 0;
+	while (pr != NULL)
+	{
+		printf("%d\n", pr->content_size);
+		int j = 0;
+		while (j < 4)
+		{
+			printf("%d - s element \n", j);
+			printf("%d\n", ((t_point*)(pr->content))[j].i);
+			printf("%d\n", ((t_point*)(pr->content))[j].j);
+			j++;
+		}
+		pr = pr->next;
+	}
 }
