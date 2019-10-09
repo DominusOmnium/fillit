088903fd4d1dcbb55ff57fa0e1f5be27be1942c1
Dake Kathleen <dkathlee@ga-t2.21-school.ru>
2019-10-09 17:58:02 +0300

    .h

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: dkathlee <dkathlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/26 13:19:25 by dkathlee          #+#    #+#             */
-/*   Updated: 2019/10/09 16:35:23 by dkathlee         ###   ########.fr       */
+/*   Updated: 2019/10/09 17:05:28 by dkathlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,16 @@ typedef struct	s_figure
 	t_point	*points;
 	int		n;
 }				t_figure;
+typedef struct	s_row
+{
+	char	*line;
+	int		n;
+}				t_row;
 
 char		**create_square(size_t size);
 void		delete_square(char ***matrix, size_t size);
 void		del_matrix(t_dlist **lst);
-int			add_row(t_dlist **matr, t_point pos, t_point *tetrimino, size_t n);
+int			add_row(t_dlist **matr, t_point pos, t_figure *tetr, size_t n);
 t_dlist		*create_matrix(size_t sq_size, t_dlist *tetr, char **sq);
 size_t		read_tetriminos(char *fname, t_dlist **figures);
 int			is_correct_figure(t_point *m_point);
