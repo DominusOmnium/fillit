a1aa5327e232f16ed008459c7faa8818e0d96d1d
Crushed Elva <celva@ga-t4.21-school.ru>
2019-10-09 14:21:00 +0300

    modif fillit.h

--- fillit.h
+++ fillit.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fillit.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dkathlee <dkathlee@student.42.fr>          +#+  +:+       +#+        */
+/*   By: celva <celva@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/26 13:19:25 by dkathlee          #+#    #+#             */
-/*   Updated: 2019/10/09 13:50:28 by dkathlee         ###   ########.fr       */
+/*   Updated: 2019/10/09 14:20:15 by celva            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,11 @@ void		del_matrix(t_dlist **lst);
 int			add_row(t_dlist **matr, t_point pos, t_point *tetrimino, size_t n);
 t_dlist		*create_matrix(size_t sq_size, t_dlist *tetr, char **sq);
 size_t		read_tetriminos(char *fname, t_dlist **figures);
+int			is_correct_figure(t_point *m_point);
+t_point		*check_simbols(char *str, t_point *m_point);
+int			check_figure(t_dlist **figures, char *str);
+void		push_tetriminos(t_dlist **figures, t_point *m_point);
+
 //t_dlist		*read_tetriminos(char *fname);
 void		pr(t_dlist *matr);
 #endif
