8d32e3465d3be0247286e955f6e1e616f83d2660
Dake Kathleen <dkathlee@ga-t2.21-school.ru>
2019-10-09 16:36:52 +0300

    follit.h

--- fillit.h
+++ fillit.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fillit.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: celva <celva@student.42.fr>                +#+  +:+       +#+        */
+/*   By: dkathlee <dkathlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/26 13:19:25 by dkathlee          #+#    #+#             */
-/*   Updated: 2019/10/09 14:20:15 by celva            ###   ########.fr       */
+/*   Updated: 2019/10/09 16:35:23 by dkathlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,14 @@ typedef struct	s_point
 	int				i;
 	int				j;
 }				t_point;
+typedef struct	s_figure
+{
+	t_point	*points;
+	int		n;
+}				t_figure;
+
 char		**create_square(size_t size);
-void		delete_square(char **matrix, size_t size);
-void		del_col(t_dlist *lst, size_t col);
-void		del_row(t_dlist **lst, size_t col);
+void		delete_square(char ***matrix, size_t size);
 void		del_matrix(t_dlist **lst);
 int			add_row(t_dlist **matr, t_point pos, t_point *tetrimino, size_t n);
 t_dlist		*create_matrix(size_t sq_size, t_dlist *tetr, char **sq);
