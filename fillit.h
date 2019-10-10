299d4973723440b69fc5216b24d3c6fccc13a3ac
Crushed Elva <celva@vo-i4.21-school.ru>
2019-10-10 11:29:33 +0300

    mod fillit.h

--- fillit.h
+++ fillit.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fillit.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dkathlee <dkathlee@student.42.fr>          +#+  +:+       +#+        */
+/*   By: celva <celva@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/26 13:19:25 by dkathlee          #+#    #+#             */
-/*   Updated: 2019/10/10 11:19:03 by dkathlee         ###   ########.fr       */
+/*   Updated: 2019/10/10 11:28:57 by celva            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ size_t		read_tetriminos(char *fname, t_dlist **figures);
 int			is_correct_figure(t_point *m_point);
 t_point		*check_simbols(char *str, t_point *m_point);
 int			heck_figure(t_dlist **figures, char *str, size_t count);
-void		push_tetriminos(t_dlist **figures, t_point *m_point);
+void		push_tetriminos(t_dlist **figures, t_point *m_point, size_t n);
 
 //t_dlist		*read_tetriminos(char *fname);
 void		pr(t_dlist *matr);
